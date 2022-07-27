a1813b19acb52c697e85057e9be3382781e7effc
William Luck <luck.william.h@gmail.com>
2022-07-27 14:13:29 -0500

    commit tree d48dfbd63d68e39a6130559217f5951a13da4dcb

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+require 'pry'
+
+def parrot(string = "Squawk!")
+    puts string
+    string
+end
+
+binding.pry
+0
